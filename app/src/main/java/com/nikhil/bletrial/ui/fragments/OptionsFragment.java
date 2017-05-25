4ebcff8f556dbef58fb00586a82e59f5e244f478
Nikhil <nikhilptambe@gmail.com>
2017-05-25 14:20:37 +0530

    Notifitcations Started

---
+++ app/src/main/java/com/nikhil/bletrial/ui/fragments/OptionsFragment.java
@@ -0,0 +1,13 @@
+package com.nikhil.bletrial.ui.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by actofitteam on 5/20/17.
+ */
+
+public class OptionsFragment extends Fragment {
+
+
+
+}
